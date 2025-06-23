6a68c162c5366193d8ceedffd082362d508e9078
Gumend3s <gustavom220405@gmail.com>
2025-06-23 18:49:17 -0300

    mergeSort(funcional)

--- Códigos C/mergeSort/main.c
+++ Códigos C/mergeSort/main.c
@@ -27,46 +27,32 @@ void mergeSort(int *vet, int start, int end)
         mergeSort(vet, mid + 1, end);
 
         mergeVet(vet, start, end, mid);
-        printf("\n");
-        for(i = 0; i < 10; i++)
-        {
-            printf("%da\n", vet[i]);
-        }
     }
 }
 
 void mergeVet(int *vet, int start, int end, int mid)
 {
-    int size = end - start;
-    if(size == 1) return;
+    int size = end - start + 1;
     int res[size];
-    int i = start, j = mid, t = 0, y;
+    int i = start, j = mid + 1, t = 0, y;
     while(i <= mid && j <= end && t < size){
         if(vet[i] < vet[j])
         {
-            res[t] = vet[i];
-            i++;
+            res[t++] = vet[i++];
         } else {
-            res[t] = vet[j];
-            j++;
-        }
-        t++;
-        printf("\ni: %d|j: %d|t: %d|size: %d\n",i, j, t, size);
-        for(y = start; y < end; y++)
-        {
-            printf("%d\n", res[y]);
+            res[t++] = vet[j++];
         }
     }
-    for(;i <= mid; i++, t++)
+    while(i <= mid)
     {
-        res[t] = vet[i];
+        res[t++] = vet[i++];
     }
-    for(;j <= end; j++, t++)
+    while(j <= end)
     {
-        res[t] = vet[j];
+        res[t++] = vet[j++];
     }
     for(t = 0; t < size; t++)
     {
-        vet[t] = res[t+start];
+        vet[t+start] = res[t];
     }
 }
