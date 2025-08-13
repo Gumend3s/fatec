58013f6ca090cff55924d038bf364f36b77bf637
Artrineias <erik21yan@gmail.com>
2025-08-13 09:14:50 -0300

    aula-13082025

---
+++ Codigos_PHP/erik/exemplo-2.php
@@ -0,0 +1,14 @@
+<?php 
+
+$lista_carro = array(
+    "Fusca"=> array("Antigo","Colecao","Manutecao facil"),
+    "Ferrari"=> array("Novo","Colecao","Caro Demais")
+);
+
+echo "<h1>Array</h1>";
+echo "<pre>";
+print_r($lista_carro);
+echo"</pre>";
+
+
+?>
