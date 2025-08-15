a7b3c5969e4ea78003c315bf8cafe5ea6edcf049
Artrineias <erik21yan@gmail.com>
2025-08-15 11:02:03 -0300

    aula15082025

---
+++ Codigos java/Exeplo_aula02/exemplo/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Aluno Amanda = new Aluno("Amanda", 25, 813164610);
+        Aluno erik = new Aluno("Erik", 26, 2015647826);
+        Aluno Gustavo = new Aluno("Gustavo", 40, 50315421);
+        Aluno luiza = new Aluno("Luiza", 21, 1646616);
+        Aluno Rodrigo = new Aluno("Rodrigo Cabello", 36, 85546852);
+
+        Amanda.Perfil();
+        erik.Perfil();
+        Gustavo.Perfil();
+        luiza.Perfil();
+        Rodrigo.Perfil();
+
+    }
+    
+
+
+
+}
+
+
+
+
+
