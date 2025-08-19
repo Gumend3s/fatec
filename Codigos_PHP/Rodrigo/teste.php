40bcfee801b7f07414dbe47691a7f2a055427c80
RCFToledo <rodrigocftoledo@gmail.com>
2025-08-19 09:02:39 -0300

    aulas php
    aulas php rodrigo

---
+++ Codigos_PHP/Rodrigo/teste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de uso do PHP</title>
+</head>
+<body>
+    <?php //inicio de bloco PHP
+    /*Comentário de múltim=plas linguas */
+    echo "Exemplo de programa PHP";
+
+    ?> <!-- fim do bloco PHP
+        comentario no HTML-->
+</body>
+</html>
