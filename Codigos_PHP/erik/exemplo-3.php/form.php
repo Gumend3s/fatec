ed0c036463cff694e02e65584e61342465d228c8
Artrineias <erik21yan@gmail.com>
2025-08-19 09:10:19 -0300

    aula19082025

---
+++ Codigos_PHP/erik/exemplo-3.php/form.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+echo "Nome:".$name;
+echo "<br>";
+echo "Email:".$email;
+echo "<br>";
+echo "Senha:" . $password;
+echo "<br>";
+
+?>
