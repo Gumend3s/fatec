4766b2e6b155c4163d0e5f6b639136bf2051f122
Artrineias <erik21yan@gmail.com>
2025-08-26 08:49:41 -0300

    260820250849

--- Codigos_PHP/aula 3/echoprint.php
+++ Codigos_PHP/aula 3/echoprint.php
@@ -1,5 +1,5 @@
 <?php 
 $valor = TRUE; 
-($valor==TRUE) ? echo "Verdadeiro" : echo " Falso"; // Não funciona 
+//($valor==TRUE) ? echo "Verdadeiro" : echo " Falso"; // Não funciona 
 ($valor==TRUE) ? print "Verdadeiro" : print " Falso"; // funciona
 ?>
