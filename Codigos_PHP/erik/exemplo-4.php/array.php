3936a6ea170343c9f15b172b90a284497b71c8d2
Artrineias <erik21yan@gmail.com>
2025-08-26 09:01:49 -0300

    260820250901

--- Codigos_PHP/erik/exemplo-4.php/array.php
+++ Codigos_PHP/erik/exemplo-4.php/array.php
@@ -1,13 +1,24 @@
 <?php
 
 
+$num1 = $_POST["num1"];
+$num2 = $_POST["num2"];
 
-$a = 10;
-$teste = ++$a ; 
+$operacao = $_POST["operacao"];
 
-
-echo $teste;
-
-echo $a;
+switch ($operacao) {
+    case "somar":
+        echo'<h1>',($num1+$num2),'</h1>';
+        break;
+    case"subtrair":
+        echo '<h1>' , ($num1 - $num2) , '</h1>';
+        break;
+    case "multiplicar":
+        echo '<h1>' , ($num1 * $num2) , '</h1>';
+        break;
+    case "dividir":
+        echo '<h1>' , ($num1 / $num2) , '</h1>';
+        break;
+}
 
 ?>
