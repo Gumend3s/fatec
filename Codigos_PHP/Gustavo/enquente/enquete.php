279b9d9f588c67e3ddb9d9b5ff44079a2e597cc2
Gumend3s <154105040+Gumend3s@users.noreply.github.com>
2025-09-09 09:14:23 -0300

    finalização ex enquete

--- Codigos_PHP/Gustavo/enquente/enquete.php
+++ Codigos_PHP/Gustavo/enquente/enquete.php
@@ -1,15 +1,29 @@
 <?php
-    $voto = $_POST["voto"];
+    $arquivo_votos = 'votos.txt';
+    $cedulas = ["PHP" => 0, "Java" => 0, "Python" => 0, "JavaScript" => 0, "C#" => 0, ];
 
-    $cedulas = array("PHP" => 0, "Java" => 0, "Python" => 0, "JavaScript" => 0, "C#" => 0, );
+    if (isset($_POST['enviar']))  {
+        $voto = $_POST["voto"];
+        
+        if (file_exists($arquivo_votos)) {
+            $cedulas = array_merge($cedulas, json_decode(file_get_contents($arquivo_votos), true));
+        }
+        $cedulas[$voto]++;
+    }
 
-    $cedulas[$voto]++;
+    if (isset($_POST['resultados'])) {
+        if (file_exists($arquivo_votos)) {
+            $cedulas = array_merge($cedulas, json_decode(file_get_contents($arquivo_votos), true));
+        }
+        foreach($cedulas as $cedula => $valor) echo $cedula , ": " , $valor , "<br>";
 
-    foreach($cedulas as $cedula => $valor) echo $cedula , ": " , $valor , "<br>";
-
-    echo '
-        <form>
-            <input type="button" value="Voltar" onclick="history.back()">
-        </form>
-        '
+        echo '
+            <form>
+                <input type="button" value="Voltar" onclick="history.back()">
+            </form>
+            ';
+    } else {
+        file_put_contents($arquivo_votos, json_encode($cedulas));
+        echo '<script>history.back();</script>';
+    }
 ?>
