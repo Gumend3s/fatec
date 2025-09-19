cec19f7c805f3cb883f21089f68f11f21fe967b4
RCFToledo <rodrigocftoledo@gmail.com>
2025-09-19 08:14:52 -0300

    n sei o que é esse comit, mas ta pedindo
    n sei o que é esse comit, mas ta pedindo

--- Codigos java/Rodrigo/Aula07/src/aula07/Aula07.java
+++ Codigos java/Rodrigo/Aula07/src/aula07/Aula07.java
@@ -1,5 +1,6 @@
 package aula07;
 
+import javax.swing.JOptionPane;
 
 public class Aula07 {
 
@@ -9,28 +10,28 @@ public class Aula07 {
         int somado, dividido;
         
         try{
-        aux = JOptionPane.showImputDialog("Digite nro1: ");
+        aux = JOptionPane.showInputDialog("Digite nro1: ");
         num1 = Integer.parseInt(aux);
 
-        aux = JOptionPane.showImputDialog("Digite nro2: ");
+        aux = JOptionPane.showInputDialog("Digite nro2: ");
         num2 = Integer.parseInt(aux);
         
         Calculadora calc = new Calculadora(num1, num2);
         
-        somado = calc.soma():
+        somado = calc.soma();
         dividido = calc.divide();
         
-        } catch (NumberFormatExeption e) {
+        } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, 
                     "Erro de conversão!\n" + 
                     e.toString(), "Erro",
                     JOptionPane.ERROR_MESSAGE);
-        } catch (ArithmeticExeption e){
+        } catch (ArithmeticException e){
             JOptionPane.showMessageDialog(null, 
                     "Erro de divisão por zero!\n" + 
                     e.toString(), "Erro",
                     JOptionPane.ERROR_MESSAGE);
-        } catch (Exeption e) {
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, 
                     "Erro genérico!\n" + 
                     e.toString(), "Erro",
