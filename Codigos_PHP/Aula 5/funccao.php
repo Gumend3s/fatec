f07af7ff438aa6793c4d35c88a3e153cdd21cac1
artrineias <erik21yan@gmail.com>
2025-11-05 08:28:54 -0300

    arrumar

--- Codigos_PHP/Aula 5/funccao.php
+++ Codigos_PHP/Aula 5/funccao.php
@@ -1,6 +1,6 @@
 <?php
 function valores($valor1="HTML", $valor2="PHP"){
-	echo "O valor1 é $valor1 e o valor2 é $valor2" . echo "<br>";
+	echo "O valor1 é $valor1 e o valor2 é $valor2" ."<br>";
 }
 valores();
 valores("CSS", "C++");
