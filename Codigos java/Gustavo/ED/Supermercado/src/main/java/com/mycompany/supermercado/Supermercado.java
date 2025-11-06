caccb67bc40e940b2ba89c78b6c8e41bdcdf8f6f
Gumend3s <154105040+Gumend3s@users.noreply.github.com>
2025-11-06 12:07:48 -0300

    ex supermercado finalizado

--- Codigos java/Gustavo/ED/Supermercado/src/main/java/com/mycompany/supermercado/Supermercado.java
+++ Codigos java/Gustavo/ED/Supermercado/src/main/java/com/mycompany/supermercado/Supermercado.java
@@ -1,7 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
+    Gustavo Mendes Oliveira
+    Rodrigo Cabello Ferraz de Toledo
+*/
 package com.mycompany.supermercado;
 
 import java.util.ArrayDeque;
@@ -15,8 +15,8 @@ public class Supermercado {
     public static void main(String[] args) {
         ArrayDeque<Cliente> filaMercado = new ArrayDeque<>();
         Cliente clienteAtual;
-        int totalMinutos = 0;
         float totalDinheiro = 0;
+        int totalMinutos = 0;
         
         filaMercado.add(new Cliente("Ana", 10, 10));
         filaMercado.add(new Cliente("Joao", 10, 15));
@@ -30,13 +30,23 @@ public class Supermercado {
         clienteAtual.setHoraatendimento(10);
         clienteAtual.setMinutoatendimento(10);
         clienteAtual.setValorgasto(150.98);
-        System.out.println( "10");
+        System.out.println("O atendimento de " + clienteAtual.getNome() + 
+                            " se iniciou as " + Integer.toString(clienteAtual.getHoraatendimento()) +
+                            ":" + Integer.toString(clienteAtual.getMinutoatendimento()) +
+                            " e durou 10 minutos, o valor gasto foi de " +
+                            Double.toString(clienteAtual.getValorgasto()));
+        totalMinutos += 10;
         totalDinheiro += clienteAtual.getValorgasto();
         
         clienteAtual = filaMercado.remove();
         clienteAtual.setHoraatendimento(10);
         clienteAtual.setMinutoatendimento(20);
         clienteAtual.setValorgasto(25.12);
+        System.out.println("O atendimento de " + clienteAtual.getNome() + 
+                            " se iniciou as " + Integer.toString(clienteAtual.getHoraatendimento()) +
+                            ":" + Integer.toString(clienteAtual.getMinutoatendimento()) +
+                            " e durou 5 minutos, o valor gasto foi de " +
+                            Double.toString(clienteAtual.getValorgasto()));
         totalMinutos += 5;
         totalDinheiro += clienteAtual.getValorgasto();
         
@@ -44,6 +54,11 @@ public class Supermercado {
         clienteAtual.setHoraatendimento(10);
         clienteAtual.setMinutoatendimento(25);
         clienteAtual.setValorgasto(627.78);
+        System.out.println("O atendimento de " + clienteAtual.getNome() + 
+                            " se iniciou as " + Integer.toString(clienteAtual.getHoraatendimento()) +
+                            ":" + Integer.toString(clienteAtual.getMinutoatendimento()) +
+                            " e durou 30 minutos, o valor gasto foi de " +
+                            Double.toString(clienteAtual.getValorgasto()));
         totalMinutos += 30;
         totalDinheiro += clienteAtual.getValorgasto();
         
@@ -51,6 +66,11 @@ public class Supermercado {
         clienteAtual.setHoraatendimento(10);
         clienteAtual.setMinutoatendimento(55);
         clienteAtual.setValorgasto(265.29);
+        System.out.println("O atendimento de " + clienteAtual.getNome() + 
+                            " se iniciou as " + Integer.toString(clienteAtual.getHoraatendimento()) +
+                            ":" + Integer.toString(clienteAtual.getMinutoatendimento()) +
+                            " e durou 10 minutos, o valor gasto foi de " +
+                            Double.toString(clienteAtual.getValorgasto()));
         totalMinutos += 10;
         totalDinheiro += clienteAtual.getValorgasto();
         
@@ -58,6 +78,11 @@ public class Supermercado {
         clienteAtual.setHoraatendimento(11);
         clienteAtual.setMinutoatendimento(05);
         clienteAtual.setValorgasto(200.67);
+        System.out.println("O atendimento de " + clienteAtual.getNome() + 
+                            " se iniciou as " + Integer.toString(clienteAtual.getHoraatendimento()) +
+                            ":" + Integer.toString(clienteAtual.getMinutoatendimento()) +
+                            " e durou 15 minutos, o valor gasto foi de " +
+                            Double.toString(clienteAtual.getValorgasto()));
         totalMinutos += 15;
         totalDinheiro += clienteAtual.getValorgasto();
         
@@ -65,15 +90,27 @@ public class Supermercado {
         clienteAtual.setHoraatendimento(11);
         clienteAtual.setMinutoatendimento(35);
         clienteAtual.setValorgasto(15.83);
-        totalMinutos += 15;
+        System.out.println("O atendimento de " + clienteAtual.getNome() + 
+                            " se iniciou as " + Integer.toString(clienteAtual.getHoraatendimento()) +
+                            ":" + Integer.toString(clienteAtual.getMinutoatendimento()) +
+                            " e durou 5 minutos, o valor gasto foi de " +
+                            Double.toString(clienteAtual.getValorgasto()));
+        totalMinutos += 5;
         totalDinheiro += clienteAtual.getValorgasto();
         
         clienteAtual = filaMercado.remove();
         clienteAtual.setHoraatendimento(12);
         clienteAtual.setMinutoatendimento(05);
         clienteAtual.setValorgasto(1200.00);
+        System.out.println("O atendimento de " + clienteAtual.getNome() + 
+                            " se iniciou as " + Integer.toString(clienteAtual.getHoraatendimento()) +
+                            ":" + Integer.toString(clienteAtual.getMinutoatendimento()) +
+                            " e durou 45 minutos, o valor gasto foi de " +
+                            Double.toString(clienteAtual.getValorgasto()));
         totalMinutos += 45;
         totalDinheiro += clienteAtual.getValorgasto();
         
+        System.out.println("O total gasto foi de R$" + Float.toString(totalDinheiro));
+        System.out.println("O total de minutos gasto foi de: " + Integer.toString(totalMinutos));
     }
 }
