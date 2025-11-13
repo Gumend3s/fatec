dc273f9fcfcc64b9a4998f947be51a6c7e46806b
Gumend3s <154105040+Gumend3s@users.noreply.github.com>
2025-11-13 10:33:16 -0300

    ex task nao finalizado

---
+++ Codigos java/Gustavo/ED/Ex_Conjunto/src/main/java/com/mycompany/ex_conjunto/Ex_Conjunto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex_conjunto;
+
+/**
+ *
+ * @author gustavo.oliveira262
+ */
+public class Ex_Conjunto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Codigos java/Gustavo/ED/Ex_Conjunto/src/main/java/com/mycompany/ex_conjunto/TaskManager.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ex_conjunto;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author gustavo.oliveira262
+ */
+public class TaskManager {
+    private Set<Task> tasks = new HashSet<>();
+    
+    public void addTask(Task t) {
+        
+    }
+    
+    public void removeTask(Task t) {
+        
+    }
+    
+    public Set<Task> getAllTasks() {
+        return tasks;
+    }
+    
+    public Task getTaskByID(int taksID) {
+        
+    }
+}
