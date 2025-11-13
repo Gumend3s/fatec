4bce29655e451ab3f205b657a7d8e33f128ce5c1
Gumend3s <154105040+Gumend3s@users.noreply.github.com>
2025-11-13 10:33:35 -0300

    Update Task.java

--- Codigos java/Gustavo/ED/Ex_Conjunto/src/main/java/com/mycompany/ex_conjunto/Task.java
+++ Codigos java/Gustavo/ED/Ex_Conjunto/src/main/java/com/mycompany/ex_conjunto/Task.java
@@ -4,10 +4,26 @@
  */
 package com.mycompany.ex_conjunto;
 
+import java.util.Objects;
+
 /**
  *
  * @author gustavo.oliveira262
  */
 public class Task {
+    private int id;
+    private String descricao;
     
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task other = (Task) o;
+        return id == other.id && Objects.equals(descricao, other.descricao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, descricao);
+    }
 }
